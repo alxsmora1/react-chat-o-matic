01c36c13d3f1d81dbc58556c2eff7e6dfe9ee115
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2020-09-11 11:26:37 -0500

    Envio de mensajes via post Se crea la interfaz para enviar los mensajes. Se crea un formulario para enviar los mensajes  mediante eventos. Se crea la función para hacer el envio de mansajes a graphql. Se crea la función para definir la quien envia el mensaje y la ui. Se crea la consulta de gql con mutation. Se hace uso de pollInterval para recibir los mensajes cada 0.5s.

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -5,9 +5,10 @@ import {
   InMemoryCache,
   ApolloProvider,
   useQuery,
-  gql
+  gql,
+  useMutation
  } from '@apollo/client';
- import { Container } from 'shards-react';
+ import { Container, Row, Col, FormInput, Button } from 'shards-react';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
@@ -24,8 +25,16 @@ query {
 }
 `;
 
+const POST_MESSAGE = gql`
+mutation ($user:String!, $content:String!) {
+  postMessage(user: $user, content: $content)
+}
+`;
+
 const Messages = ({ user }) => {
-  const { data } = useQuery(GET_MESSAGES);
+  const { data } = useQuery(GET_MESSAGES, {
+    pollInterval: 500,
+  });
   if (!data) {
     return null;
   }
@@ -38,11 +47,25 @@ const Messages = ({ user }) => {
         <div
           style={{
             display: 'flex',
-            jsutifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+            justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
             paddingBottom: "1em",
           }}
           >
-            {user !== messageUser}
+            {user !== messageUser && (
+              <div
+                style={{
+                  height: 50,
+                  width: 50,
+                  marginRight: "0.5em",
+                  border: "2px solid #e5e6ea",
+                  borderRadius: 50,
+                  textAlign: "center",
+                  fontSize: "12pt",
+                  paddingTop: 9,
+                }}>
+                {messageUser.slice(0,3).toUpperCase()}
+              </div>
+            )}
             <div style={{
               background: user === messageUser ? "#58bf56" : "#e5e6ea",
               color: user === messageUser ? "white" : "black",
@@ -59,8 +82,59 @@ const Messages = ({ user }) => {
 }
 
 const Chat = () => {
+  const [state, stateSet] = React.useState({
+    user: 'Alexis',
+    content: '',
+  });
+  const [postMessage] = useMutation(POST_MESSAGE);
+  const onSend = () => {
+    if (state.content.length > 0) {
+      postMessage({
+        variables: state,
+      });
+    }
+
+    stateSet({
+      ...state,
+      content : "",
+    });
+  }
   return (
-    <Container><Messages user="Cecilia" /></Container>
+    <Container>
+      <Messages user={state.user} />
+      <Row>
+        <Col xs={2} style={{ padding: 0}}>
+        <FormInput
+          label="User"
+          value={state.user}
+          onChange={(evt) => stateSet({
+            ...state,
+            user: evt.target.value,
+          })}
+        />
+        </Col>
+        <Col xs={8} style={{ padding: 0}}>
+        <FormInput
+          label="Message"
+          value={state.content}
+          onChange={(evt) => stateSet({
+            ...state,
+            content: evt.target.value,
+          })}
+          onKeyUp={(evt) => {
+            if (evt.keyCode === 13) {
+              onSend();
+            }
+          }}
+        />
+        </Col>
+        <Col xs={2} style={{ padding: 0 }}>
+          <Button onClick={() => onSend()}>
+            Send
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
