fe0711d229b3b29ab1071bade2742a6a1d54b5b3
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2020-09-17 23:27:49 -0500

    Agregado websocket y subscripciones Se crea un enlace entre graphql y el chat para hacer uso de las subscripciones. Se cambia la query por la subscripción, para mantener el acceso al  websocket.

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -4,19 +4,28 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  useQuery,
   gql,
-  useMutation
+  useMutation,
+  useSubscription,
  } from '@apollo/client';
- import { Container, Row, Col, FormInput, Button } from 'shards-react';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { Container, Row, Col, FormInput, Button } from 'shards-react';
+
+const link = new WebSocketLink({
+  uri: `ws://localhost:4000/`,
+  options: {
+    reconnect:  true
+  },
+});
 
 const client = new ApolloClient({
+  link,
   uri: 'http://localhost:4000',
   cache: new InMemoryCache()
 });
 
 const GET_MESSAGES = gql`
-query {
+subscription {
   messages {
     id
     content
@@ -32,9 +41,7 @@ mutation ($user:String!, $content:String!) {
 `;
 
 const Messages = ({ user }) => {
-  const { data } = useQuery(GET_MESSAGES, {
-    pollInterval: 500,
-  });
+  const { data } = useSubscription(GET_MESSAGES);
   if (!data) {
     return null;
   }
